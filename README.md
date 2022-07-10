7177f6b42efaa0b353ccf9ef1b9418ba711b1b95
NateDogg <natmays97@gmail.com>
2022-07-10 18:32:02 -0500

    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Challenge 03
+
+With this challenge, I had many ideas and tried many different approaches. The final product is the cleanest and most efficient code I could find to successfully generate a random password.
+
+I first added all characters into different string depending on user preference. I separated regular, special and capital characters into seperate strings, and created a seperate string to include all characters to make it a bit easier to work with.
+
+I then wrote a series of prompts using window commands to gather data about what kind of password the user wants to generate.
+
+I then confined the number of characters to between 8 and 128 to meet acceptance criteria. 
+
+Next I wrote a separate function (perhaps this was unnecessary) to keep the page neat an organized. The purpose of the function  genChar() is to generate a number of random characters within the selected strings depending on the values submitted by the user.
+
+I used code I found online that uses a for-loop addition assignment combined with the charAt function to achieve this goal.
--- script.js
+++ script.js
@@ -1,5 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Adding all normal characters
+var characters = "abcdefghijklmnopqrstuvwxyz1234567890";
+// Adding all special characters
+var specialCharacters = "~`!@#$%^&*()_+-={}|[]:<>?,./";
+// Converting all characters to uppercase and using it seperately
+var upperCharacters = characters.toUpperCase();
+// Combining all characters including upper case and special characters
+var allCharacters = characters + specialCharacters + upperCharacters;
+
+var generatePassword = function(){
+  var specChar = window.confirm("Would you like to include special characters? Click cancel for no.");
+  var caseChar = window.confirm("Would you like your password to be case sensitive? Click cancel for no.");
+  var numChar  = window.prompt("How many characters would you like to use? (128 max)");
+
+// This conditional statement confines numChar to 8 - 128 and generates a random
+// number between 8 - 128 if no imput is entered
+
+  if (!numChar){
+    var numChar = Math.floor(Math.random() * (128 - 8) + 8);
+  } else if (numChar < 8){
+    var numChar = 8;
+  } else if (numChar > 128){
+    var numChar = 128;
+  }
+
+// This function generates a string of random characters the length of numChar
+
+  function genChar(numChar){
+    var result = '';
+    for ( var i=0; i<numChar; i++ ){
+
+// The conditional statement uses the information gathered from the prompts
+// to select from the available characters    
+
+    if (!specChar && !caseChar){        
+        result += characters.charAt(Math.floor(Math.random() * 
+        characters.length));          
+    }
+
+    else if (specChar && !caseChar){          
+        result += (characters+specialCharacters).charAt(Math.floor(Math.random() * 
+        (characters+specialCharacters).length));           
+    }
+
+    else if (!specChar && caseChar){          
+        result += (characters+upperCharacters).charAt(Math.floor(Math.random() * 
+        (characters+upperCharacters).length));           
+    }
+
+    else {          
+        result += allCharacters.charAt(Math.floor(Math.random() * 
+        allCharacters.length));           
+    }
+    }
+    return result;
+  };
+
+  return genChar(numChar);
+};
+
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +68,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
